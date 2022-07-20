bde43ad845a5662ba0f254538951367a46b95ce0
akashguptasky <skygupta193@gmail.com>
2022-07-20 18:32:47 +0530

    Redis or caching imp

--- route/route.js
+++ route/route.js
@@ -9,4 +9,11 @@ router.post('/url/shorten',shortUrlController.shortUrl)
 
 router.get('/:urlCode', getUrlController.getUrl)
 
+router.all("/**", function (req, res) {
+    res.status(404).send({
+        status: false,
+        msg: "The api you requested is not available"
+    })
+})
+
 module.exports= router;
